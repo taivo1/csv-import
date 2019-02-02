bb521a19ab13a16a2149a11972fb28740bdf7dcd
Taivo <taivok@gmail.com>
2019-02-02 18:43:36 +0200

    bootstrap react, update babel and modify webpack conf

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Pipedrive FS Task</h1>
+        );
+    }
+}
+
+export default App;
